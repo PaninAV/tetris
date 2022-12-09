4c81c9a2a20c3723d9167fc04533eaceeacb7318
ASUS <tamik03092003@gmail.com>
2022-12-09 11:24:05 +0300

    1.0

--- main.py
+++ main.py
@@ -4,15 +4,7 @@ import music
 import environment
 import saves
 
-path = "pepe.mp4"
-
 pygame.init()
-
-menu_state = "main"
-
-music_volume = 0.5
-sound_volume = 0.5
-pygame.mixer.music.set_volume(music_volume)
 
 
 def text_objects(text, font_edit):
@@ -22,7 +14,7 @@ def text_objects(text, font_edit):
 
 class Button:
 
-    def __init__(self, message, x, y, width, height, inactive_color, active_color, action):
+    def __init__(self, message, x, y, width, height, inactive_color, active_color):
         self.message = message
         self.x = x
         self.y = y
@@ -30,7 +22,6 @@ class Button:
         self.height = height
         self.inactive_color = inactive_color
         self.active_color = active_color
-        self.action = action
 
     def print_text(self):
         text_surf, text_rect = text_objects(self.message, environment.menu_font)
@@ -41,16 +32,18 @@ class Button:
         if self.x + self.width > mouse[0] > self.x and self.y + self.height > mouse[1] > self.y:
             pygame.draw.rect(screen, self.active_color, (self.x, self.y, self.width, self.height))
             if click[0] == 1:
-                self.action()
+                self.print_text()
+                return True
         else:
             pygame.draw.rect(screen, self.inactive_color, (self.x, self.y, self.width, self.height))
         self.print_text()
+        return False
 
 
 class GameButton(Button):
 
-    def __init__(self,  message, x, y, width, height, inactive_color, active_color, action, level):
-        super().__init__(message, x, y, width, height, inactive_color, active_color, action)
+    def __init__(self,  message, x, y, width, height, inactive_color, active_color, level):
+        super().__init__(message, x, y, width, height, inactive_color, active_color)
         self.level = level
 
     def set_button_color(self, level_status):
@@ -58,15 +51,6 @@ class GameButton(Button):
             self.active_color = environment.red
         else:
             self.active_color = environment.green
-
-    def show_button(self):
-        if self.x + self.width > mouse[0] > self.x and self.y + self.height > mouse[1] > self.y:
-            pygame.draw.rect(screen, self.active_color, (self.x, self.y, self.width, self.height))
-            if click[0] == 1:
-                self.action(self.level)
-        else:
-            pygame.draw.rect(screen, self.inactive_color, (self.x, self.y, self.width, self.height))
-        self.print_text()
 
 
 class Figure:
@@ -195,9 +179,6 @@ class Tetris:
 
 def start(game):
     music.play_music(music.music_for_levels[game.level - 1])
-    global difficult
-    global event
-    global lives
     done = False
     pause = False
     pressing_down = False
@@ -205,7 +186,7 @@ def start(game):
     counter = 0
 
     text_level = environment.font3.render("Level: " + str(game.level), True, environment.BLACK)
-    text_lives = environment.font3.render("Lives: " + str(lives), True, environment.BLACK)
+    text_lives = environment.font3.render("Lives: " + str(menu.lives), True, environment.BLACK)
     text_open = environment.font2.render("You open " + str(game.level + 1) + " level!", True, (75, 0, 130))
 
     while not done:
@@ -218,15 +199,15 @@ def start(game):
         if counter > 100000:
             counter = 0
 
-        if counter % (11 - (difficult + 1) - game.level) == 0 or pressing_down:
+        if counter % (11 - (menu.difficulty + 1) - game.level) == 0 or pressing_down:
             if game.state == "play":
                 game.go_down()
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
                 done = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_ESCAPE:
                     pygame.mixer.music.pause()
                     if pause is False:
                         game.state = "pause"
@@ -235,23 +216,24 @@ def start(game):
                         game.state = "play"
                         pause = False
                 if game.state == "play":
-                    if event.key == pygame.K_UP:
+                    if e.key == pygame.K_UP:
                         game.rotate()
-                    if event.key == pygame.K_DOWN:
+                    if e.key == pygame.K_DOWN:
                         pressing_down = True
-                    if event.key == pygame.K_LEFT:
+                    if e.key == pygame.K_LEFT:
                         game.go_side(-1)
-                    if event.key == pygame.K_RIGHT:
+                    if e.key == pygame.K_RIGHT:
                         game.go_side(1)
-                    if event.key == pygame.K_SPACE:
+                    if e.key == pygame.K_SPACE:
                         game.go_space()
-                if event.key == pygame.K_RETURN:
+                if e.key == pygame.K_RETURN:
                     if game.state == "Gameover":
-                        lives -= 1
+                        menu.lives -= 1
                     done = True
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_DOWN:
+            if e.type == pygame.KEYUP:
+                if e.key == pygame.K_DOWN:
                     pressing_down = False
+
         screen.fill(environment.WHITE)
         if game.state == "play" or game.state == "pause":
             for i in range(game.height):
@@ -299,16 +281,16 @@ def start(game):
             if game_goes is True:
                 game_goes = False
                 pygame.mixer.music.stop()
-                if lives == 1:
+                if menu.lives == 1:
                     music.lose_sound.play()
                 else:
                     music.sad_sound.play()
-            if lives > 1:
+            if menu.lives > 1:
                 screen.blit(environment.pepe, (0, 0))
             else:
                 screen.blit(environment.master, (0, 0))
 
-        if game.score >= game.level * 5 * (difficult + 1):
+        if game.score >= game.level * 5 * (menu.difficulty + 1):
             screen.blit(environment.text_congratulation, (15, 300))
             screen.blit(environment.text_continue, (125, 600))
             if game.level == 5:
@@ -317,10 +299,12 @@ def start(game):
                 screen.blit(text_open, (100, 400))
             if game.state == "play":
                 music.win_sound.play()
-                music.play_music(music.music_for_levels[4])
+                music.play_music(music.music_for_levels[5])
                 game.state = "Complete"
+
         if game.state == "pause":
             screen.blit(environment.menu_font.render("PAUSE", True, environment.BLACK), [200, 150])
+
         pygame.display.flip()
         clock.tick(fps)
     music.random_music_for_menu()
@@ -336,156 +320,171 @@ fps = 30
 
 
 def set_true(button_level):
-    global accepts
     if start(Tetris(20, 10, button_level)) == "Complete":
         if button_level < 5:
-            saves.set_new_save(difficult, button_level - 1)
-            accepts = saves.set_saves(accepts)
-            print(accepts)
+            saves.set_new_save(menu.difficulty, button_level - 1)
+            menu.set_accepts()
+            print(menu.accepts)
 
 
 def start_level(button_level):
     if button_level != 1:
-        if accepts[difficult][button_level - 2] is True:
+        if menu.accepts[menu.difficulty][button_level - 2] is True:
             set_true(button_level)
     else:
             set_true(button_level)
 
 
-def back():
-    global menu_state
-    menu_state = "main"
-
-
-def increase_music_volume():
-    global music_volume
-    music_volume = music.increase_volume(music_volume)
-    pygame.mixer.music.set_volume(music_volume)
-
-
-def decrease_music_volume():
-    global music_volume
-    music_volume = music.decrease_volume(music_volume)
-    pygame.mixer.music.set_volume(music_volume)
-
-
-def increase_sound_volume():
-    global sound_volume
-    sound_volume = music.increase_volume(sound_volume)
-    music.new_sound_volume(sound_volume)
-
-
-def decrease_sound_volume():
-    global sound_volume
-    sound_volume = music.decrease_volume(sound_volume)
-    music.new_sound_volume(sound_volume)
-
-
-def increase_difficult():
-    global difficult
-    if difficult <= 2:
-        difficult += 1
-
-
-def decrease_difficult():
-    global difficult
-    if difficult >= 1:
-        difficult -= 1
-
-
 def show_play_menu():
-    first_level_button.show_button()
-    second_level_button.set_button_color(accepts[difficult][0])
-    second_level_button.show_button()
-    third_level_button.set_button_color(accepts[difficult][1])
-    third_level_button.show_button()
-    forth_level_button.set_button_color(accepts[difficult][2])
-    forth_level_button.show_button()
-    fifth_level_button.set_button_color(accepts[difficult][3])
-    fifth_level_button.show_button()
-    back_button.show_button()
+    if first_level_button.show_button() is True:
+        start_level(1)
+    second_level_button.set_button_color(menu.accepts[menu.difficulty][0])
+    if second_level_button.show_button() is True:
+        start_level(2)
+    third_level_button.set_button_color(menu.accepts[menu.difficulty][1])
+    if third_level_button.show_button() is True:
+        start_level(3)
+    forth_level_button.set_button_color(menu.accepts[menu.difficulty][2])
+    if forth_level_button.show_button() is True:
+        start_level(4)
+    fifth_level_button.set_button_color(menu.accepts[menu.difficulty][3])
+    if fifth_level_button.show_button() is True:
+        start_level(5)
+    if back_button.show_button() is True:
+        menu.set_main_menu()
 
 
 def show_menu():
-    start_button.show_button()
-    options_button.show_button()
-    exit_button.show_button()
+    if start_button.show_button() is True:
+        menu.set_play_menu()
+    if options_button.show_button() is True:
+        menu.set_settings()
+    if exit_button.show_button() is True:
+        quit_game()
 
 
 def show_settings():
-
-    music_volume_text = environment.menu_font.render("Volume: " + str(int(round(music_volume * 10, 0))), True,
+    music_volume_text = environment.menu_font.render("Volume: " + str(int(round(menu.music_volume * 10, 0))), True,
                                                      environment.BLACK)
-    sounds_volume_text = environment.menu_font.render("Volume: " + str(int(round(sound_volume * 10, 0))), True,
+    sounds_volume_text = environment.menu_font.render("Volume: " + str(int(round(menu.sound_volume * 10, 0))), True,
                                                       environment.BLACK)
     screen.blit(music_volume_text, [175, 150])
     screen.blit(sounds_volume_text, [175, 250])
     screen.blit(environment.text_sound, [200, 205])
     screen.blit(environment.text_music, [210, 105])
-    increase_sound_button.show_button()
-    decrease_sound_button.show_button()
-    increase_music_button.show_button()
-    decrease_music_button.show_button()
-    increase_difficult_button.show_button()
-    decrease_difficult_button.show_button()
+    if increase_sound_button.show_button() is True:
+        menu.increase_sound_volume()
+    if decrease_sound_button.show_button() is True:
+        menu.decrease_sound_volume()
+    if increase_music_button.show_button() is True:
+        menu.increase_music_volume()
+    if decrease_music_button.show_button() is True:
+        menu.decrease_music_volume()
+    if increase_difficult_button.show_button() is True:
+        menu.increase_difficult()
+    if decrease_difficult_button.show_button() is True:
+        menu.decrease_difficult()
     difficult_text = environment.menu_font.render("Difficulty: " + find_difficult(), True, environment.BLACK)
     screen.blit(difficult_text, (135, 350))
-    new_game_button.show_button()
-    back_button.show_button()
-    #
+    if new_game_button.show_button() is True:
+        saves.set_zero_saves()
+    if back_button.show_button() is True:
+        menu.set_main_menu()
+
+
+difficult_slovar = {0: 'Easy', 1: 'Norm', 2: 'Hard', 3: 'HELL'}
+
 
 def find_difficult():
-    return difficult_slovar.get(difficult)
+    return difficult_slovar.get(menu.difficulty)
 
 
-def set_settings():
-    global menu_state
-    menu_state = "settings"
+class Menu:
 
+    state = "main"
+    lives = 3
+    difficulty = 0
+    music_volume = 0.5
+    sound_volume = 0.5
+    accepts = [
+        [False, False, False, False],
+        [False, False, False, False],
+        [False, False, False, False],
+        [False, False, False, False],
+    ]
 
-def set_play_menu():
-    global menu_state
-    menu_state = "play menu"
+    def __init__(self, state, diffuculty, lives, music_volume, sound_volume):
+        self.state = state
+        self.difficulty = diffuculty
+        self.lives = lives
+        self.music_volume = music_volume
+        self.sound_volume = sound_volume
+        self.set_accepts()
+
+    def set_settings(self):
+        self.state = "settings"
+
+    def set_play_menu(self):
+        self.state = "play menu"
+
+    def set_main_menu(self):
+        self.state = "main"
+
+    def increase_difficult(self):
+        if self.difficulty <= 2:
+            self.difficulty += 1
+
+    def decrease_difficult(self):
+        if self.difficulty >= 1:
+            self.difficulty -= 1
+
+    def increase_music_volume(self):
+        self.music_volume = music.increase_volume(self.music_volume)
+        pygame.mixer.music.set_volume(self.music_volume)
+
+    def decrease_music_volume(self):
+        self.music_volume = music.decrease_volume(self.music_volume)
+        pygame.mixer.music.set_volume(self.music_volume)
+
+    def increase_sound_volume(self):
+        self.sound_volume = music.increase_volume(self.sound_volume)
+        music.new_sound_volume(self.sound_volume)
+
+    def decrease_sound_volume(self):
+        self.sound_volume = music.decrease_volume(self.sound_volume)
+        music.new_sound_volume(self.sound_volume)
+
+    def set_accepts(self):
+        self.accepts = saves.set_saves(self.accepts)
 
 
 def quit_game():
-    global accepts
-    accepts = saves.set_saves(accepts)
     pygame.quit()
     quit()
 
 
-start_button = Button("GO!", 200, 150, 200, 50, environment.BLACK, environment.green, set_play_menu)
-exit_button = Button("Quit", 200, 350, 200, 50, environment.BLACK, environment.red, quit_game)
-options_button = Button("Options", 200, 250, 200, 50, environment.BLACK, environment.yellow, set_settings)
-increase_music_button = Button("+", 475, 150, 50, 50, environment.BLACK, environment.green, increase_music_volume)
-decrease_music_button = Button("-", 75, 150, 50, 50, environment.BLACK, environment.red, decrease_music_volume)
-increase_sound_button = Button("+", 475, 250, 50, 50, environment.BLACK, environment.green, increase_sound_volume)
-decrease_sound_button = Button("-", 75, 250, 50, 50, environment.BLACK, environment.red, decrease_sound_volume)
-increase_difficult_button = Button("+", 475, 350, 50, 50, environment.BLACK, environment.green, increase_difficult)
-decrease_difficult_button = Button("-", 75, 350, 50, 50, environment.BLACK, environment.red, decrease_difficult)
-back_button = Button("Back", 200, 550, 200, 50, environment.BLACK, environment.red, back)
-first_level_button = GameButton("Level 1", 200, 25, 200, 50, environment.BLACK, environment.green, start_level, 1)
-second_level_button = GameButton("Level 2", 200, 125, 200, 50, environment.BLACK, environment.green, start_level, 2)
-third_level_button = GameButton("Level 3", 200, 225, 200, 50, environment.BLACK, environment.green, start_level, 3)
-forth_level_button = GameButton("Level 4", 200, 324, 200, 50, environment.BLACK, environment.green, start_level, 4)
-fifth_level_button = GameButton("Level 5", 200, 425, 200, 50, environment.BLACK, environment.green, start_level, 5)
-new_game_button = Button("New game", 175, 450, 250, 55, environment.BLACK, environment.yellow,
-                         saves.set_zero_saves)
+start_button = Button("GO!", 200, 150, 200, 50, environment.BLACK, environment.green)
+exit_button = Button("Quit", 200, 350, 200, 50, environment.BLACK, environment.red)
+options_button = Button("Options", 200, 250, 200, 50, environment.BLACK, environment.yellow)
+increase_music_button = Button("+", 475, 150, 50, 50, environment.BLACK, environment.green)
+decrease_music_button = Button("-", 75, 150, 50, 50, environment.BLACK, environment.red)
+increase_sound_button = Button("+", 475, 250, 50, 50, environment.BLACK, environment.green)
+decrease_sound_button = Button("-", 75, 250, 50, 50, environment.BLACK, environment.red)
+increase_difficult_button = Button("+", 475, 350, 50, 50, environment.BLACK, environment.green)
+decrease_difficult_button = Button("-", 75, 350, 50, 50, environment.BLACK, environment.red)
+back_button = Button("Back", 200, 550, 200, 50, environment.BLACK, environment.red)
+first_level_button = GameButton("Level 1", 200, 25, 200, 50, environment.BLACK, environment.green, 1)
+second_level_button = GameButton("Level 2", 200, 125, 200, 50, environment.BLACK, environment.green, 2)
+third_level_button = GameButton("Level 3", 200, 225, 200, 50, environment.BLACK, environment.green, 3)
+forth_level_button = GameButton("Level 4", 200, 324, 200, 50, environment.BLACK, environment.green, 4)
+fifth_level_button = GameButton("Level 5", 200, 425, 200, 50, environment.BLACK, environment.green, 5)
+new_game_button = Button("New game", 175, 450, 250, 55, environment.BLACK, environment.yellow)
 
+menu = Menu("main", 0, 3, 0.5, 0.5)
+pygame.mixer.music.set_volume(menu.music_volume)
 play = False
-accepts = [
-    [False, False, False, False],
-    [False, False, False, False],
-    [False, False, False, False],
-    [False, False, False, False],
-]
-accepts = saves.set_saves(accepts)
-lives = 3
-difficult = 0
-number_of_music_menu = music.random_music_for_menu()
 
-difficult_slovar = {0: 'Easy', 1: 'Norm', 2: 'Hard', 3: 'HELL'}
+number_of_music_menu = music.random_music_for_menu()
 
 while not play:
 
@@ -498,18 +497,18 @@ while not play:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             play = True
-        if lives == 0:
+        if menu.lives == 0:
             saves.set_zero_saves()
-            accepts = saves.set_saves(accepts)
-            lives = 3
+            menu.accepts = saves.set_saves(menu.accepts)
+            menu.lives = 3
         click = pygame.mouse.get_pressed()
         mouse = pygame.mouse.get_pos()
 
-        if menu_state == "main":
+        if menu.state == "main":
             show_menu()
-        elif menu_state == "settings":
+        elif menu.state == "settings":
             show_settings()
-            accepts = saves.set_saves(accepts)
+            menu.accepts = saves.set_saves(menu.accepts)
         else:
             show_play_menu()
         pygame.display.update()
